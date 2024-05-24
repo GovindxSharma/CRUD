2294576345a1222ce21753a5969abfe2f955272f
GovindxSharma <149459294+GovindxSharma@users.noreply.github.com>
2024-05-24 17:33:55 +0530

    DB name added

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,17 @@
-import mongoose from "mongoose"
-const Connection= async(username,password)=>{
-    const URL=`mongodb+srv://${username}:${password}@crud-app.9kzvdlv.mongodb.net/?retryWrites=true&w=majority`;
-    try{
-        
-         await mongoose.connect(URL);
+import mongoose from "mongoose";
 
-        console.log('Database connected Successfully')
-    }catch(error){
-        console.log('Error while connecting to DataBase',error)
+const Connection = async (username, password) => {
+    const dbName = process.env.MONGODB_NAME; // Retrieve the database name from environment variables
+    const URL = `mongodb+srv://${username}:${password}@crud-app.9kzvdlv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(URL, {
+            useNewUrlParser: true, // These options are optional but recommended for better handling of MongoDB connections
+            useUnifiedTopology: true
+        });
+        console.log('Database connected Successfully');
+    } catch (error) {
+        console.log('Error while connecting to DataBase', error);
     }
 }
 
